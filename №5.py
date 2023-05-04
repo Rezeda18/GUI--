4695bcd51cec99ea93fa5bdb6edf3587b9018eb6
Rezeda18 <124900709+Rezeda18@users.noreply.github.com>
2023-05-04 17:08:33 +0300

    Delete №5.py

--- №5.py
+++
@@ -1,9 +0,0 @@
-kol = int(input('Введите количество слов, которое собираетесь ввести: '))
-text = ''
-
-for i in range(1, kol + 1):
-    word = input()
-    text = text + word + ' '
-    word =''
-
-print(text)
